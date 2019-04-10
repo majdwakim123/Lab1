7f0cb4f9dbd7e97fc05202d157d8af59c091ebd8
SamKanjow <32685926+SamKanjow@users.noreply.github.com>
2019-04-10 12:16:19 +0200

    Sam

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        int a= 1, b=2, c;
-        c = b-a;
-        System.out.println(c);
+
     }
 }
