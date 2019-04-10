b147fa15cf30e4c2feaf0115d0b617bff84d5ea3
MAWA0155 <majedwakim123@hotmail.com>
2019-04-10 12:06:04 +0200

    Majd comment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-
+        int a= 1, b=2, c;
+        c = b-a;
+        System.out.println(c);
     }
 }
