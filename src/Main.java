afad6611c1a978d42f4db4d77388a308642b6480
SamKanjow <32685926+SamKanjow@users.noreply.github.com>
2019-04-10 12:08:56 +0200

    updating Value of C

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         int a= 1, b=2, c;
-        c = b-a;
+        c = (b + a);
         System.out.println(c);
     }
 }
